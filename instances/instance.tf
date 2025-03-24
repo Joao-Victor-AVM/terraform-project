58d50d07b7934ea0b9816034d2b4230bdb873b34
Joao-Victor-AVM <almeidamelo2012@gmail.com>
2025-03-24 19:12:08 -0300

    Correção do ultimo commit

--- instances/instance.tf
+++ instances/instance.tf
@@ -133,3 +133,22 @@ resource "aws_db_instance" "jrlb_jvavm_RDS" {
     Periodo = "8"
   }
 }
+
+resource "aws_security_group" "sg_alb" {
+  name        = "sg-alb"
+  description = "Security Group for ALB"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
