991dc3ff6739a88feb96964596d40fe957a7e820
Joao-Victor-AVM <almeidamelo2012@gmail.com>
2025-03-24 18:52:36 -0300

    Limpeza da Máquina

--- vpc/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-    backend "s3" {
-        key = "terraform/tfstate.tfstate"
-        bucket = "tfstate-jrlb-jvavm"
-        region = "us-east-1"
-    }
-}
--- vpc/bucket.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "jrlb_jvavm_state"{
-    bucket = "tfstate-jrlb-jvavm"
-
-    tags = {
-        Aluno = "jrlb_jvavm"
-        Periodo = "8"
-    }
-}
